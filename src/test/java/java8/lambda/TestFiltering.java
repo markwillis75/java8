df8e94cc4140ac9150743d82c00be156e2a4a362
Mark Willis <Mark Willis>
2019-10-21 22:03:22 +0100

    Test negate/or/and predicate chaining

--- src/test/java/java8/lambda/TestFiltering.java
+++ src/test/java/java8/lambda/TestFiltering.java
@@ -1,8 +1,8 @@
 package java8.lambda;
 
-import java8.lambda.Filtering;
 import java8.pojo.*;
 import org.junit.*;
+import static org.junit.Assert.*;
 
 import java.util.*;
 import java.util.function.Predicate;
@@ -30,39 +30,39 @@ public class TestFiltering {
         assertColor(redApples, AppleColor.RED);
 
         List<Apple> greenApples = filterByColor(AppleColor.GREEN);
-        Assert.assertTrue(greenApples.size() == 2);
+        assertTrue(greenApples.size() == 2);
         assertColor(greenApples, AppleColor.GREEN);
 
-        Assert.assertNotEquals(redApples, greenApples);
+        assertNotEquals(redApples, greenApples);
     }
 
     @Test
     public void testFilterByWeight(){
         List<Apple> w10 = filterByWeight(10);
-        Assert.assertTrue(w10.size() == 1);
+        assertTrue(w10.size() == 1);
 
         List<Apple> w25 = filterByWeight(25);
-        Assert.assertTrue(w25.size() == 1);
+        assertTrue(w25.size() == 1);
 
         List<Apple> w90 = filterByWeight(90);
-        Assert.assertTrue(w90.size() == 2);
+        assertTrue(w90.size() == 2);
 
         List<Apple> w99 = filterByWeight(99);
-        Assert.assertTrue(w99.size() == 0);
+        assertTrue(w99.size() == 0);
     }
 
     @Test
     public void testFilterByWeightAndColor(){
         List<Apple> red90  = filterByWeightAndColor(90, AppleColor.RED);
-        Assert.assertTrue(red90.size() == 1);
-        Assert.assertTrue(red90.get(0).getColor().equals(AppleColor.RED) && red90.get(0).getWeight() == 90);
+        assertTrue(red90.size() == 1);
+        assertTrue(red90.get(0).getColor().equals(AppleColor.RED) && red90.get(0).getWeight() == 90);
 
         List<Apple> green90 = filterByWeightAndColor(90, AppleColor.GREEN);
-        Assert.assertTrue(green90.size() == 1);
-        Assert.assertTrue(green90.get(0).getColor().equals(AppleColor.GREEN) && red90.get(0).getWeight() == 90);
+        assertTrue(green90.size() == 1);
+        assertTrue(green90.get(0).getColor().equals(AppleColor.GREEN) && red90.get(0).getWeight() == 90);
 
         List<Apple> green10 = filterByWeightAndColor(10, AppleColor.GREEN);
-        Assert.assertTrue(green10.isEmpty());
+        assertTrue(green10.isEmpty());
     }
 
     @Test
@@ -72,12 +72,51 @@ public class TestFiltering {
         List<Apple> filterApples = filter.filterApples(apples, pp);
         List<Apple> filterGeneric = filter.filterAnything(apples, pp);
 
-        Assert.assertEquals(filterApples, filterGeneric);
+        assertEquals(filterApples, filterGeneric);
     }
+
+    @Test
+    public void testNegatedPredicate(){
+        Predicate<Apple> pp = (a) -> a.getWeight() > 50;
+        List<Apple> lessThan50 = filter.filterApples(apples,
+                pp.negate());
+
+        assertTrue(lessThan50.size() == 2);
+
+        for(Apple apple: lessThan50){
+            assertTrue(apple.getWeight() < 50);
+        }
+    }
+
+    @Test
+    public void tetLogicalOr(){
+        Predicate<Apple> weight10 = (a) -> a.getWeight() == 10;
+        Predicate<Apple> weight25 = (a) -> a.getWeight() == 25;
+
+        List<Apple> weight10or25 = filter.filterApples(apples, weight10.or(weight25));
+
+        assertTrue(weight10or25.size() == 2);
+
+        for (Apple apple: weight10or25){
+            assertTrue(apple.getWeight() == 10 || apple.getWeight() == 25);
+        }
+    }
+
+    @Test
+    public void testAnd(){
+        Predicate<Apple>  red = (a) -> a.getColor().equals(AppleColor.RED);
+        Predicate<Apple>  heavierThan10 = (a) -> a.getWeight() > 10;
+
+        List<Apple> redAndHeavy = filter.filterApples(apples, red.and(heavierThan10));
+
+        assertTrue(redAndHeavy.size() == 1);
+        assertTrue(redAndHeavy.get(0).getWeight() > 10 && redAndHeavy.get(0).getColor().equals(AppleColor.RED));
+    }
+
 
     private void assertColor(List<Apple> apples, AppleColor color){
         for (Apple apple:apples){
-            Assert.assertTrue(apple.getColor().equals(color));
+            assertTrue(apple.getColor().equals(color));
         }
     }
 
